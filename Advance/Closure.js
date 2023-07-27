dd4618604e8ed6b4f9ba34da7d42a224d3f1036e
Ange230700 <angeericstephane.kouakou@ynov.com>
2023-07-27 09:43:36 +0200

    learning how to do a closure

--- Advance/Closure.js
+++ Advance/Closure.js
@@ -1,5 +1,7 @@
-// A closure is an inner function that has access to the outer (enclosing) function’s variables—scope chain.
-//The closure has three scope chains (its own scope,outer function’s variables,global variables).
+/* 
+A closure is an inner function that has access to the outer (enclosing) function’s variables—scope chain.
+The closure has three scope chains (its own scope,outer function’s variables,global variables).
+*/
 
 function showName(firstName, lastName) {
     var nameIntro = "Your name is :";
@@ -7,10 +9,10 @@ function showName(firstName, lastName) {
     // this inner function has access to the outer function's variables, including the parameter​.
 
     function makeFullName() {
-        return nameIntro + firstName + " " + lastName;
+        return nameIntro + " " + firstName + " " + lastName;
     }
 
     return makeFullName();
 }
 
-showName("michael", "action");
+console.log(showName("Michael", "Jackson")); // Your name is :michael Jackson
