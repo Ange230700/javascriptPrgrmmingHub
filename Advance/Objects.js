9b2e618dd096e8b27c78f00a823632b3389efdb8
Ange230700 <kouakouangeericstephane@gmail.com>
2023-08-24 20:09:01 +0200

    Refactoring...

--- Advance/Objects.js
+++ Advance/Objects.js
@@ -1,9 +1,9 @@
 // A JavaScript object is an unordered collection of variables called named values.
 
 var person = {
-    firstname: "Bob",
-    lastname: "Marley",
+    firstName: "Bob",
+    lastName: "Marley",
     age: 50
 };
 
-console.log("My name is " + person.firstname + " " + person.lastname + ". I am " + person.age + " years old.");
+console.log(`My name is ${person.firstName} ${person.lastName}. I am ${person.age} years old.`);
