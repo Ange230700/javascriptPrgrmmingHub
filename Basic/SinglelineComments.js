5eb960ac7692f9589a6c95f476ad17dc96a19faf
Ange230700 <kouakouangeericstephane@gmail.com>
2023-08-24 21:32:51 +0200

    Refactoring...

---
+++ Basic/SinglelineComments.js
@@ -0,0 +1,4 @@
+// Write to a heading:
+document.getElementById("myHeader").innerHTML = "Welcome to JavaScript";
+// Write to a paragraph:
+document.getElementById("myParagraph").innerHTML = "This is a paragraph.";
